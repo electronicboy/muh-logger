5b82542fd1930305e18e78380ab944419be2b8a5
Shane Freeder <theboyetronic@gmail.com>
2022-05-21 04:35:03 +0100

    run earlier, "support" minecraft logger, and reloading

--- src/main/java/pw/valaria/muhlogger/Muhlogger.java
+++ src/main/java/pw/valaria/muhlogger/Muhlogger.java
@@ -4,15 +4,22 @@ import com.destroystokyo.paper.utils.PaperPluginLogger;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
-import org.apache.logging.log4j.LogManager;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.PluginClassLoader;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -20,56 +27,106 @@ import java.util.logging.Logger;
 public final class Muhlogger extends JavaPlugin {
 
     List<String> loggers;
+    List<Logger> injected = new ArrayList<>();
 
     @Override
-    public void onEnable() {
+    public void onLoad() {
         saveDefaultConfig();
         reloadConfig();
-        // Plugin startup logic
-        loggers.forEach((loggerName) -> {
-            // Faux a PDF, the logic this calls literally doesn't care
-            final Logger logger = PaperPluginLogger.getLogger(new PluginDescriptionFile(loggerName, "unknown", "unknown"));
-
-            final Handler[] handlers = logger.getHandlers();
-            for (Handler handler : handlers) {
-                if (handler.getClass().getClassLoader() instanceof PluginClassLoader classLoader) {
-                    if (classLoader.getPlugin().getName().equals(this.getName())) {
-                        getLogger().removeHandler(handler);
-                    }
-                }
-            }
-            logger.addHandler(new Handler() {
-                                       @Override
-                                       public void publish(LogRecord record) {
-                                           final TextComponent deserialize = LegacyComponentSerializer.legacySection().deserialize(record.getMessage());
-                                           Bukkit.getConsoleSender().sendMessage(Component.text("[" + record.getLoggerName() + "] ").append(deserialize));
-                                       }
-
-                                       @Override
-                                       public void flush() {
-
-                                       }
-
-                                       @Override
-                                       public void close() throws SecurityException {
-
-                                       }
-                                   }
-            );
-            logger.setUseParentHandlers(false);
-        });
-
     }
 
     @Override
     public void reloadConfig() {
+        doRemove();
         super.reloadConfig();
 
         loggers = this.getConfig().getStringList("loggers");
+        doApply();
+    }
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 0) {
+            return false;
+        }
+
+        if ("reload".equals(args[0].toLowerCase(Locale.ROOT))) {
+            reloadConfig();
+            sender.sendMessage(Component.text("Reloaded muh logger!", NamedTextColor.RED));
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (args.length == 0 || args.length == 1) {
+            return List.of("reload");
+        } else return List.of();
+    }
+
+    private void doApply() {
+        loggers.forEach((loggerName) -> {
+            // Faux a PDF, the logic this calls literally doesn't care
+            final Logger logger = PaperPluginLogger.getLogger(new PluginDescriptionFile(loggerName, "unknown", "unknown"));
+            applyHandler(logger);
+        });
+    }
+
+    private void applyHandler(Logger logger) {
+
+        final Handler[] handlers = logger.getHandlers();
+        for (Handler handler : handlers) {
+            if (handler.getClass().getClassLoader() instanceof PluginClassLoader classLoader) {
+                if (classLoader.getPlugin().getName().equals(this.getName())) {
+                    getLogger().removeHandler(handler);
+                }
+            }
+        }
+        logger.addHandler(new Handler() {
+                              @Override
+                              public void publish(LogRecord record) {
+                                  final TextComponent deserialize = LegacyComponentSerializer.legacySection().deserialize(record.getMessage());
+                                  Component prefix;
+                                  if (record.getLoggerName().equals("Minecraft")) {
+                                      prefix = Component.text().asComponent();
+                                  } else {
+                                      prefix = Component.text("[" + record.getLoggerName() + "] ");
+                                  }
+                                  Bukkit.getConsoleSender().sendMessage(prefix.append(deserialize));
+                              }
+
+                              @Override
+                              public void flush() {
+
+                              }
+
+                              @Override
+                              public void close() throws SecurityException {
+
+                              }
+                          }
+        );
+        logger.setUseParentHandlers(false);
+        injected.add(logger);
+    }
+
+    private void doRemove() {
+        final Iterator<Logger> iterator = injected.iterator();
+
+        while (iterator.hasNext()) {
+            final Logger logger = iterator.next();
+            final Handler[] handlers = logger.getHandlers();
+            for (Handler handler : handlers) {
+                if (handler.getClass().getClassLoader() instanceof PluginClassLoader classLoader) {
+                    if (classLoader.getPlugin().getName().equals(this.getName())) {
+                        logger.removeHandler(handler);
+                    }
+                }
+            }
+            logger.setUseParentHandlers(true);
+            iterator.remove();
+        }
     }
 }
